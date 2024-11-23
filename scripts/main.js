047e1bda63495c289d8e9d6421a13b5b25562b2b
Bullet-Crystal <elhouarisamad@gmail.com>
2024-11-23 22:56:10 +0100

    a

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,16 @@
 
 const listCours = document.querySelector('.listCours');
 const listTd = document.querySelector('.listTd');
-const CoursSelector = document.querySelector('.CoursSelector');
-const TdSelector = document.querySelector('.TdSelector');
+const coursSelector = document.querySelector('.CoursSelector');
+const tdSelector = document.querySelector('.TdSelector');
+const singupForm = document.getElementById('singupForm');
+
+let Users = localStorage.getItem("Users");
+if (!Users) {
+    Users = {};
+} else {
+    Users = JSON.parse(Users);
+}
 
 
 document.addEventListener('click',()=>{
@@ -11,12 +19,34 @@ document.addEventListener('click',()=>{
     listCours.style.display = 'none';
 })
 
-CoursSelector.addEventListener('click',(event)=>{
+coursSelector.addEventListener('click',(event)=>{
     event.stopPropagation();
     listCours.style.display = listCours.style.display == 'block'?'none':'block';
 })
 
-TdSelector.addEventListener('click',(event)=>{
+tdSelector.addEventListener('click',(event)=>{
     event.stopPropagation();
     listTd.style.display = listTd.style.display == 'block'?'none':'block';
 })
+
+singupForm.onsubmit = function (event){
+    event.preventDefault();
+    let form = new FormData(singupForm);
+    Name = form.get('Nom');
+    FamilyName = form.get('Prénom');
+    Email = form.get('Email');
+    Password = form.get('Password');
+    Confirm = form.get('Confirm');
+    if(Password === Confirm){
+        Users[Email] = {
+            'Name':Name,
+            'FamilyName':FamilyName,
+            'Password':Password
+        }
+        localStorage.setItem("Users",JSON.stringify(Users));
+        alert('Votre Compte à été creer avec succés');
+        location.href = 'login.html';
+    }
+    else
+        alert("Le mot de passe n'est pas le meme ");
+};
